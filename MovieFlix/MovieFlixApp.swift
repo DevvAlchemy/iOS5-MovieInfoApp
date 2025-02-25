2c6153f3b237810186968b6ab94ca0b5e87c0bff
Carlito Kabambi <carlito.kabambi@triosstudent.com>
2025-02-24 22:12:43 -0500

    Initial Commit

---
+++ MovieFlix/MovieFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixApp.swift
+//  MovieFlix
+//
+//  Created by Royal K on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieFlixTests/MovieFlixTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixTests.swift
+//  MovieFlixTests
+//
+//  Created by Royal K on 2025-02-24.
+//
+
+import Testing
+@testable import MovieFlix
+
+struct MovieFlixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
